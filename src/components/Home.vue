8c8afee6119a04c6cefde03b1684360941a8eb68
marunchao <yixialei0215@163.com>
2020-12-15 15:12:44 +0800

    完成了登录退出功能

---
+++ src/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <el-button type="info" @click="logout">退出</el-button>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        logout(){
+            this.$http.post('/account/logout/', {}).then((data)=>{
+                if(data.data.tip === 0){
+                    window.sessionStorage.clear()
+                    this.$router.push('/login')
+                }
+            })
+
+        }
+    },
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,31 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 手动配置element-ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+// 导入全局样式
+import './assets/css/global.css'
+// 导入字体图标
+import './assets/fonts/iconfont.css'
+import axios from 'axios'
+axios.defaults.baseURL = 'http://localhost:8000'
+// 添加请求拦截器
+let axios_ob = axios.create({
+  baseURL: 'http://localhost:8000'
+});
 
+axios_ob.interceptors.request.use((config) => {
+  const tokenStr = sessionStorage.getItem('token')
+  if (tokenStr){
+    config.headers['Authorization'] = tokenStr
+  }
+  return config
+}, (err) => {
+  return Promise.reject(err)
+})
+Vue.prototype.$http = axios_ob
 Vue.config.productionTip = false
 
 new Vue({
